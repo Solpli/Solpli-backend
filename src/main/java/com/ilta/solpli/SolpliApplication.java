d6b17eff4aafc659d80886c1f8593a10067621b1
gyuseon <mn5592@naver.com>
2025-04-06 00:46:49 +0900

    Chore: 초기 세팅 (#1)

---
+++ src/main/java/com/ilta/solpli/SolpliApplication.java
@@ -0,0 +1,12 @@
+package com.ilta.solpli;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SolpliApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SolpliApplication.class, args);
+  }
+}
---
+++ src/test/java/com/ilta/solpli/SolpliApplicationTests.java
@@ -0,0 +1,12 @@
+package com.ilta.solpli;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+import org.junit.jupiter.api.Test;
+
+@SpringBootTest
+class SolpliApplicationTests {
+
+  @Test
+  void contextLoads() {}
+}
