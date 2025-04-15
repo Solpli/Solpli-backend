4371b4abc59078dab1b0afadba32c38183aaf232
gyuseon <mn5592@naver.com>
2025-04-15 22:27:10 +0900

    Feat: Place 엔티티 생성 (#18)

---
+++ src/main/java/com/ilta/solepli/domain/place/entity/Place.java
@@ -0,0 +1,52 @@
+package com.ilta.solepli.domain.place.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import com.ilta.solepli.global.entity.Timestamped;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Builder
+@Table(name = "places")
+public class Place extends Timestamped {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  @Column(nullable = false)
+  private String name;
+
+  @Column(nullable = false)
+  private String address;
+
+  @Column(nullable = false)
+  private String district;
+
+  @Column(nullable = false)
+  private String neighborhood;
+
+  @Column(nullable = false)
+  private String latitude;
+
+  @Column(nullable = false)
+  private String longitude;
+
+  @Column(nullable = false, name = "google_place_id")
+  private String googlePlaceId;
+
+  @Column(nullable = false)
+  private String types;
+}
