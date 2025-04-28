cb7ae3fd28778661300ab8737cef263e233021f8
gyuseon <mn5592@naver.com>
2025-04-28 15:34:35 +0900

    Feat: 인증 관련 CustomUserDetailService 추가 (#20)

--- src/main/java/com/ilta/solepli/domain/user/repository/UserRepository.java
+++ src/main/java/com/ilta/solepli/domain/user/repository/UserRepository.java
@@ -1,7 +1,11 @@
 package com.ilta.solepli.domain.user.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.ilta.solepli.domain.user.entity.User;
 
-public interface UserRepository extends JpaRepository<User, Long> {}
+public interface UserRepository extends JpaRepository<User, Long> {
+  Optional<User> findByLoginId(String loginId);
+}
--- src/main/java/com/ilta/solepli/global/exception/ErrorCode.java
+++ src/main/java/com/ilta/solepli/global/exception/ErrorCode.java
@@ -8,7 +8,10 @@ import lombok.Getter;
 public enum ErrorCode {
 
   // 에러코드 예시: 샘플 에러
-  SAMPLE_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "예시: 샘플 에러가 발생했습니다.");
+  SAMPLE_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "예시: 샘플 에러가 발생했습니다."),
+
+  // 유저 관련 에러
+  USER_NOT_FOUND(HttpStatus.NOT_FOUND, "사용자 정보가 존재하지 않습니다.");
 
   private final HttpStatus httpStatus;
   private final String message;
