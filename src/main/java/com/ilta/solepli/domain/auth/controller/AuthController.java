675b16aed6dad9d54d2557ba47ca4c95b42f39b3
gyuseon <mn5592@naver.com>
2025-05-02 10:06:58 +0900

    Refactor: 공통응답코드 적용 (#20)

--- src/main/java/com/ilta/solepli/domain/auth/controller/AuthController.java
+++ src/main/java/com/ilta/solepli/domain/auth/controller/AuthController.java
@@ -1,6 +1,5 @@
 package com.ilta.solepli.domain.auth.controller;
 
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -18,6 +17,7 @@ import lombok.RequiredArgsConstructor;
 import com.ilta.solepli.domain.auth.dto.request.BasicLoginRequest;
 import com.ilta.solepli.domain.auth.dto.response.LoginResponse;
 import com.ilta.solepli.domain.auth.service.AuthService;
+import com.ilta.solepli.global.response.SuccessResponse;
 
 @RestController
 @RequiredArgsConstructor
@@ -31,29 +31,30 @@ public class AuthController {
       summary = "기본 회원가입 API",
       description = "(관리자) 아이디와 비밀번호로로 회원가입합니다. 로그인에 사용할 아이디와 비밀번호를 입력해주세요.")
   @PostMapping("/signup")
-  public ResponseEntity<Void> signUp(@Valid @RequestBody BasicLoginRequest basicLoginRequest) {
+  public ResponseEntity<SuccessResponse<Void>> signUp(
+      @Valid @RequestBody BasicLoginRequest basicLoginRequest) {
 
     authService.signup(basicLoginRequest);
-    return ResponseEntity.status(HttpStatus.OK).build();
+    return ResponseEntity.ok().body(SuccessResponse.successWithNoData("회원가입 성공"));
   }
 
   @Operation(
       summary = "기본 로그인 API",
       description = "(관리자) 아이디와 비밀번호로 로그인합니다. 로그인에 사용할 아이디와 비밀번호를 입력해주세요.")
   @PostMapping("/login")
-  public ResponseEntity<LoginResponse> login(
+  public ResponseEntity<SuccessResponse<LoginResponse>> login(
       @Valid @RequestBody BasicLoginRequest basicLoginRequest) {
 
     LoginResponse loginResponse = authService.login(basicLoginRequest);
-    return ResponseEntity.status(HttpStatus.OK).body(loginResponse);
+    return ResponseEntity.ok().body(SuccessResponse.successWithData(loginResponse));
   }
 
   @Operation(summary = "소셜 로그인 API", description = "소셜 로그인을 진행합니다. (카카오, 네이버, 구글) 인가코드를 넣어주세요.")
   @GetMapping("/login/{loginType}")
-  public ResponseEntity<LoginResponse> login(
+  public ResponseEntity<SuccessResponse<LoginResponse>> login(
       @PathVariable String loginType, @RequestParam String code) {
 
     LoginResponse loginResponse = authService.socialLogin(code, loginType);
-    return ResponseEntity.status(HttpStatus.OK).body(loginResponse);
+    return ResponseEntity.ok().body(SuccessResponse.successWithData(loginResponse));
   }
 }
