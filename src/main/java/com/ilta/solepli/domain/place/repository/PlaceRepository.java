7b4a180d92311c70b86443a30fdd7e0fede9bbc9
byeonghoon <qudgns119@naver.com>
2025-05-14 20:40:21 +0900

    Feat: 좌표 안에 속하는 장소 리스트 조회 메서드 구현 (#23)

--- src/main/java/com/ilta/solepli/domain/place/repository/PlaceRepository.java
+++ src/main/java/com/ilta/solepli/domain/place/repository/PlaceRepository.java
@@ -1,7 +1,27 @@
 package com.ilta.solepli.domain.place.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.ilta.solepli.domain.place.entity.Place;
 
-public interface PlaceRepository extends JpaRepository<Place, Long> {}
+public interface PlaceRepository extends JpaRepository<Place, Long> {
+
+  @Query(
+      "SELECT p "
+          + "FROM Place p "
+          + "JOIN p.placeCategories pc "
+          + "JOIN pc.category c "
+          + "WHERE p.latitude BETWEEN :swLat AND :neLat "
+          + "AND p.longitude BETWEEN :swLng AND :neLng "
+          + "AND (:category IS NULL OR c.name = :category)")
+  List<Place> findInViewportWithOptionalCategory(
+      @Param("swLat") Double swLat,
+      @Param("swLng") Double swLng,
+      @Param("neLat") Double neLat,
+      @Param("neLng") Double neLng,
+      @Param("category") String category);
+}
