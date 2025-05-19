9a6928029753577ab6ab72e87f59786c6c351687
gyuseon <mn5592@naver.com>
2025-05-19 15:39:11 +0900

    Refactor: 쏠렉트 삭제 방식 SoftDelete으로 변경으로 인한 쿼리 수정 (#26)

--- src/main/java/com/ilta/solepli/domain/solelect/repository/SolelectRepository.java
+++ src/main/java/com/ilta/solepli/domain/solelect/repository/SolelectRepository.java
@@ -4,13 +4,12 @@ import java.util.Optional;
 
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import com.ilta.solepli.domain.solelect.entity.Solelect;
 
 public interface SolelectRepository extends JpaRepository<Solelect, Long> {
   @EntityGraph(attributePaths = {"user", "solelectContents"})
-  @Query("SELECT s FROM Solelect s WHERE s.id = :id AND s.deleteAt IS NULL")
+  //  @Query("SELECT s FROM Solelect s WHERE s.id = :id AND s.deletedAt IS NULL")
   Optional<Solelect> findWithContentById(@Param("id") Long id);
 }
