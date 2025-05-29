bc01273095840a529c77d3d90b3c7d18fa2b317a
byeonghoon <qudgns119@naver.com>
2025-05-29 16:26:18 +0900

    Feat: 입력받은 지역이름을 기반으로 장소 조회 메서드 추가 (#43)

--- src/main/java/com/ilta/solepli/domain/place/repository/PlaceRepository.java
+++ src/main/java/com/ilta/solepli/domain/place/repository/PlaceRepository.java
@@ -2,14 +2,13 @@ package com.ilta.solepli.domain.place.repository;
 
 import java.util.List;
 
-import org.springframework.context.annotation.Primary;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import com.ilta.solepli.domain.place.entity.Place;
 
-@Primary
 public interface PlaceRepository extends JpaRepository<Place, Long>, PlaceRepositoryCustom {
 
   @Query(
@@ -26,4 +25,13 @@ public interface PlaceRepository extends JpaRepository<Place, Long>, PlaceReposi
       @Param("neLat") Double neLat,
       @Param("neLng") Double neLng,
       @Param("category") String category);
+
+  @EntityGraph(attributePaths = {"placeCategories", "placeCategories.category"})
+  @Query(
+      "SELECT DISTINCT  p "
+          + "FROM Place p "
+          + "JOIN FETCH p.placeCategories pc "
+          + "JOIN FETCH pc.category c "
+          + "WHERE p.district = :regionName OR p.neighborhood = :regionName")
+  List<Place> findAllByRegionName(@Param("regionName") String regionName);
 }
