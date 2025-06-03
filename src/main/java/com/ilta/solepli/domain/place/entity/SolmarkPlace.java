8b5ca03a898d411625e4645785116d64c2e08a64
byeonghoon <qudgns119@naver.com>
2025-06-03 16:21:14 +0900

    Feat: 쏠마크 장소 엔티티 추가 (#53)

---
+++ src/main/java/com/ilta/solepli/domain/place/entity/SolmarkPlace.java
@@ -0,0 +1,32 @@
+package com.ilta.solepli.domain.place.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+
+import com.ilta.solepli.domain.user.entity.User;
+import com.ilta.solepli.global.entity.Timestamped;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Builder
+@Table(name = "solmark_places")
+public class SolmarkPlace extends Timestamped {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "user_id")
+  @OnDelete(action = OnDeleteAction.CASCADE)
+  private User user;
+
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "place_id")
+  @OnDelete(action = OnDeleteAction.CASCADE)
+  private Place place;
+}
