ec9b50a1eb1a03d0e108a4c85b0f7e8704ff9a66
byeonghoon <qudgns119@naver.com>
2025-06-03 16:23:38 +0900

    Feat: 쏠마크 장소 판별하는 반환 데이터 추가 (#53)

--- src/main/java/com/ilta/solepli/domain/solmap/dto/MarkerResponse.java
+++ src/main/java/com/ilta/solepli/domain/solmap/dto/MarkerResponse.java
@@ -3,4 +3,5 @@ package com.ilta.solepli.domain.solmap.dto;
 import lombok.Builder;
 
 @Builder
-public record MarkerResponse(Long id, Double latitude, Double longitude, String category) {}
+public record MarkerResponse(
+    Long id, Double latitude, Double longitude, String category, Boolean solmark) {}
