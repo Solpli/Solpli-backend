56088605d446823d2111362a1aec8bf88511bc99
byeonghoon <qudgns119@naver.com>
2025-06-11 21:37:27 +0900

    Feat: 장소 리스트별 최대 장소 저장 수 검증 시 N + 1 방지를 위한 Fetch join 추가 (#79)

--- src/main/java/com/ilta/solepli/domain/solmark/place/repository/SolmarkPlaceCollectionRepository.java
+++ src/main/java/com/ilta/solepli/domain/solmark/place/repository/SolmarkPlaceCollectionRepository.java
@@ -3,6 +3,7 @@ package com.ilta.solepli.domain.solmark.place.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.ilta.solepli.domain.solmark.place.entity.SolmarkPlaceCollection;
 import com.ilta.solepli.domain.user.entity.User;
@@ -12,5 +13,12 @@ public interface SolmarkPlaceCollectionRepository
 
   Integer countByUser(User user);
 
+  @Query(
+      """
+    SELECT DISTINCT spc
+    FROM SolmarkPlaceCollection spc
+    JOIN FETCH spc.solmarkPlaces sp
+    WHERE spc.user = :user AND spc.id IN :collectionIds
+""")
   List<SolmarkPlaceCollection> findByUserAndId_In(User user, List<Long> collectionIds);
 }
