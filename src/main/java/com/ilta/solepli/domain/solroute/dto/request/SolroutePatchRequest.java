09c50a8ae4e3ed4c0453adbb99d9b0bf4a62d0d2
gyuseon <mn5592@naver.com>
2025-06-20 16:52:26 +0900

    Feat: 쏠루트 수정 요청에 장소정보 Nullable로 변경 (#97)

--- src/main/java/com/ilta/solepli/domain/solroute/dto/request/SolroutePatchRequest.java
+++ src/main/java/com/ilta/solepli/domain/solroute/dto/request/SolroutePatchRequest.java
@@ -11,10 +11,7 @@ import lombok.Builder;
 public record SolroutePatchRequest(
     Integer iconId,
     @Size(max = 25, message = "제목은 25자 이하로 입력해주세요.") String name,
-    @NotNull(message = "장소 정보는 필수입니다.")
-        @Size(max = 50, message = "장소 정보는 최대 50개까지 등록할 수 있습니다.")
-        @Valid
-        List<PlaceInfo> placeInfos) {
+    @Size(max = 50, message = "장소 정보는 최대 50개까지 등록할 수 있습니다.") @Valid List<PlaceInfo> placeInfos) {
   public record PlaceInfo(
       @NotNull(message = "장소 ID는 필수입니다.") Long placeId,
       @NotNull(message = "순서는 필수입니다.") Integer seq,
